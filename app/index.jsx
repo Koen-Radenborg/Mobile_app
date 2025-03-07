9d34130a2e17fb8704ccf8e09a6016ad6ca444d2
ScootTheDuck <9021648@student.zadkine.nl>
2025-03-07 14:05:57 +0100

    actual progress omlordy

---
+++ app/index.jsx
@@ -0,0 +1,76 @@
+import { ImageBackground, Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+
+const App = () => {
+    const router = useRouter();
+
+    return (
+        <ImageBackground style={styles.background} source={require('../assets/images/Landing-Background-1.png')}>
+            <View style={styles.overlay}>
+                <Image source={require('../assets/images/Logo.png')} style={styles.logo} />
+                <Text style={styles.title}>Explore a
+                    {'\n'}new world
+                    {'\n'}with us
+                </Text>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={() => router.push('/screens/auth/register')}>
+                        <Text style={styles.buttonText}>REGISTER</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => router.push('/screens/auth/login')}>
+                        <Text style={styles.buttonText}>LOGIN</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ImageBackground>
+    );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        filter: 'brightness(1.5)',
+    },
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        justifyContent: 'space-between',
+        padding: 20,
+        paddingBottom: 50,
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+        position: 'absolute',
+        top: 40,
+        left: 20,
+    },
+    title: {
+        fontSize: 50,
+        color: '#fff',
+        lineHeight: 50,
+        marginTop: 375,
+    },
+    buttonContainer: {
+        width: '100%',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    button: {
+        width: '100%',
+        backgroundColor: '#000',
+        padding: 15,
+        borderRadius: 15,
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
+
+export default App;
